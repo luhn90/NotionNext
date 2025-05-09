a95a98de82ef9de05fa8c22b71096a74c5b591fe
luhn90 <113449002+luhn90@users.noreply.github.com>
2025-05-09 21:58:19 +0800

    Update config.js
    网址多输入一个分号

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
 
   LANDING_HEADER_BUTTON_1_TITLE: 'Github开源',
-  LANDING_HEADER_BUTTON_1_URL: ''https://passer1024.xyz',
+  LANDING_HEADER_BUTTON_1_URL: 'https://passer1024.xyz',
 
   LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
-  LANDING_HEADER_BUTTON_2_URL: ''https://passer1024.xyz',
+  LANDING_HEADER_BUTTON_2_URL: 'https://passer1024.xyz',
 
   // 首页大图英雄板块
   LANDING_HERO_TITLE_1: 'NotionNext',
