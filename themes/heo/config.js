f32f3660339c82acc515b967081c849bc3922557
luhn90 <113449002+luhn90@users.noreply.github.com>
2025-05-10 15:43:15 +0800

    Update config.js
    修正访问地址（passer024.xyz→passer1024.xyz）

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://passer024.xyz',
+  HEO_INFO_CARD_URL2: 'https://passer1024.xyz',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://passer024.xyz',
+  HEO_INFO_CARD_URL3: 'https://passer1024.xyz',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -116,7 +116,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
